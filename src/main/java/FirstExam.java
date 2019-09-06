4ac64c4776bb52c9f54cfaabc389e44f0e5c2641
seung min <3235tmdals@gmail.com>
2019-09-06 22:53:06 +0900

    async subject practice

--- src/main/java/FirstExam.java
+++ src/main/java/FirstExam.java
@@ -1,13 +1,14 @@
 import io.reactivex.Observable;
+import io.reactivex.subjects.AsyncSubject;
 
 public class FirstExam {
-    public void emit(){
-        Observable.just("Hello", "RxJava 2!!")
-                .subscribe(System.out::println);
-    }
-
     public static void main(String[] args) {
-        FirstExam demo = new FirstExam();
-        demo.emit(); 
+        AsyncSubject<String> subject = AsyncSubject.create();
+        subject.subscribe(data -> System.out.println("Subscriber #1 => " + data));
+        subject.onNext("1");
+        subject.onNext("3");
+        subject.subscribe(data -> System.out.println("Subscriber #2 => " + data));
+        subject.onNext("5");
+        subject.onComplete();
     }
 }
