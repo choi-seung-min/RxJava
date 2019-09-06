6a26fd6b00ceb4409a57b4f4d5eb656eed8b9921
seung min <3235tmdals@gmail.com>
2019-09-06 22:58:35 +0900

    async subject as subscriber

--- src/main/java/FirstExam.java
+++ src/main/java/FirstExam.java
@@ -3,12 +3,13 @@ import io.reactivex.subjects.AsyncSubject;
 
 public class FirstExam {
     public static void main(String[] args) {
-        AsyncSubject<String> subject = AsyncSubject.create();
+        Float[] temp = {10.4f, 13.4f, 12.5f};
+        Observable<Float> source = Observable.fromArray(temp);
+
+
+        AsyncSubject<Float> subject = AsyncSubject.create();
         subject.subscribe(data -> System.out.println("Subscriber #1 => " + data));
-        subject.onNext("1");
-        subject.onNext("3");
-        subject.subscribe(data -> System.out.println("Subscriber #2 => " + data));
-        subject.onNext("5");
-        subject.onComplete();
+
+        source.subscribe(subject);
     }
 }
