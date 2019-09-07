808902c205cc99c0d2a27913c852c42e772c7503
seung min <3235tmdals@gmail.com>
2019-09-07 22:14:56 +0900

    behavior subject class

--- src/main/java/FirstExam.java
+++ src/main/java/FirstExam.java
@@ -1,16 +1,15 @@
 import io.reactivex.Observable;
 import io.reactivex.subjects.AsyncSubject;
+import io.reactivex.subjects.BehaviorSubject;
 
 public class FirstExam {
     public static void main(String[] args) {
-        AsyncSubject<Integer> subject = AsyncSubject.create();
-        subject.onNext(10);
-        subject.onNext(11);
+        BehaviorSubject<String> subject = BehaviorSubject.createDefault("6");
         subject.subscribe(data -> System.out.println("Subscriber #1 => " + data));
-        subject.onNext(12);
-        subject.onComplete();
-        subject.onNext(13);
+        subject.onNext("1");
+        subject.onNext("3");
         subject.subscribe(data -> System.out.println("Subscriber #2 => " + data));
-        subject.subscribe(data -> System.out.println("Subscriber #3 => " + data));
+        subject.onNext("5");
+        subject.onComplete();
     }
 }
