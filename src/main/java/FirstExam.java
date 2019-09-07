4482b5d0a9b1938508f830089b4bb037064ba0af
seung min <3235tmdals@gmail.com>
2019-09-07 22:30:27 +0900

    publishSubject and replaySubject

--- src/main/java/FirstExam.java
+++ src/main/java/FirstExam.java
@@ -1,15 +1,27 @@
 import io.reactivex.Observable;
 import io.reactivex.subjects.AsyncSubject;
 import io.reactivex.subjects.BehaviorSubject;
+import io.reactivex.subjects.PublishSubject;
+import io.reactivex.subjects.ReplaySubject;
 
 public class FirstExam {
     public static void main(String[] args) {
-        BehaviorSubject<String> subject = BehaviorSubject.createDefault("6");
-        subject.subscribe(data -> System.out.println("Subscriber #1 => " + data));
-        subject.onNext("1");
-        subject.onNext("3");
-        subject.subscribe(data -> System.out.println("Subscriber #2 => " + data));
-        subject.onNext("5");
-        subject.onComplete();
+        // PublishSubject example
+        PublishSubject<String> publishSubject = PublishSubject.create();
+        publishSubject.subscribe(data -> System.out.println("PublishSubject Subscriber #1 => " + data));
+        publishSubject.onNext("1");
+        publishSubject.onNext("3");
+        publishSubject.subscribe(data -> System.out.println("PublishSubject Subscriber #2 => " + data));
+        publishSubject.onNext("5");
+        publishSubject.onComplete();
+
+        // ReplaySubject example
+        ReplaySubject<String> replaySubject = ReplaySubject.create();
+        replaySubject.subscribe(data -> System.out.println("ReplaySubject Subscriber #1 => " + data));
+        replaySubject.onNext("1");
+        replaySubject.onNext("3");
+        replaySubject.subscribe(data -> System.out.println("ReplaySubject Subscriber #2 => " + data));
+        replaySubject.onNext("5");
+        replaySubject.onComplete();
     }
 }
