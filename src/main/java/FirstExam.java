7836c3c518c48bcc714df785da192ec1c0579669
seung min <3235tmdals@gmail.com>
2019-09-07 23:42:28 +0900

    using map

--- src/main/java/FirstExam.java
+++ src/main/java/FirstExam.java
@@ -1,27 +1,20 @@
 import io.reactivex.Observable;
-import io.reactivex.subjects.AsyncSubject;
-import io.reactivex.subjects.BehaviorSubject;
-import io.reactivex.subjects.PublishSubject;
-import io.reactivex.subjects.ReplaySubject;
+import io.reactivex.observables.ConnectableObservable;
+
+import java.util.concurrent.TimeUnit;
 
 public class FirstExam {
     public static void main(String[] args) {
-        // PublishSubject example
-        PublishSubject<String> publishSubject = PublishSubject.create();
-        publishSubject.subscribe(data -> System.out.println("PublishSubject Subscriber #1 => " + data));
-        publishSubject.onNext("1");
-        publishSubject.onNext("3");
-        publishSubject.subscribe(data -> System.out.println("PublishSubject Subscriber #2 => " + data));
-        publishSubject.onNext("5");
-        publishSubject.onComplete();
-
-        // ReplaySubject example
-        ReplaySubject<String> replaySubject = ReplaySubject.create();
-        replaySubject.subscribe(data -> System.out.println("ReplaySubject Subscriber #1 => " + data));
-        replaySubject.onNext("1");
-        replaySubject.onNext("3");
-        replaySubject.subscribe(data -> System.out.println("ReplaySubject Subscriber #2 => " + data));
-        replaySubject.onNext("5");
-        replaySubject.onComplete();
+        String[] balls = {"RED", "YELLOW", "GREEN", "BLUE"};
+        Observable<Integer> source = Observable.fromArray(balls).map(data -> {
+            switch (data){
+                case "RED": return 1;
+                case "YELLOW": return 2;
+                case "GREEN": return 3;
+                case "BLUE": return 5;
+                default: return -1;
+            }
+        });
+        source.subscribe(System.out::println);
     }
 }
