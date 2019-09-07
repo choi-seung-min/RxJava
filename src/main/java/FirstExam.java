83858b0c2d52a2007f1c1a6b08dcda4f4ed9c04f
seung min <3235tmdals@gmail.com>
2019-09-07 23:49:43 +0900

    using flat map returning observable

--- src/main/java/FirstExam.java
+++ src/main/java/FirstExam.java
@@ -5,16 +5,9 @@ import java.util.concurrent.TimeUnit;
 
 public class FirstExam {
     public static void main(String[] args) {
-        String[] balls = {"RED", "YELLOW", "GREEN", "BLUE"};
-        Observable<Integer> source = Observable.fromArray(balls).map(data -> {
-            switch (data){
-                case "RED": return 1;
-                case "YELLOW": return 2;
-                case "GREEN": return 3;
-                case "BLUE": return 5;
-                default: return -1;
-            }
-        });
+        String[] balls = {"1", "3", "5"};
+        Observable<String> source = Observable.fromArray(balls)
+                .flatMap(data -> Observable.just(data + "<>", data + "<>"));
         source.subscribe(System.out::println);
     }
 }
