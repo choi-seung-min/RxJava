04b094463277efa593a0940977d9b9cd848d471a
seung min <3235tmdals@gmail.com>
2019-09-08 14:55:08 +0900

    filter and others

--- src/main/java/FirstExam.java
+++ src/main/java/FirstExam.java
@@ -1,19 +1,44 @@
 import io.reactivex.Observable;
+import io.reactivex.Single;
+import io.reactivex.internal.operators.observable.ObservableError;
 
 import java.util.Scanner;
 
 public class FirstExam {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("input");
-        int dan = Integer.parseInt(in.nextLine());
-
-//        Observable<String> source = Observable.just(dan)
-//                .flatMap(num -> Observable.range(1, 9)
-//                .map(row -> num + "*" + row + "=" + num*row));
-        Observable<String> source = Observable.just(dan)
-                .flatMap(gugu -> Observable.range(1, 9),
-                        (gugu, i) -> gugu + "*" + i + "=" + gugu*i);
+        String[] obj = {"1 CIRCLE", "2 DIAMOND", "3 TRIANGLE", "4 DIAMOND", "5 CIRCLE", "6 HEXAGON"};
+        Observable<String> source = Observable.fromArray(obj).filter(data -> data.endsWith("CIRCLE"));
         source.subscribe(System.out::println);
+
+        Integer[] obj2 = {100, 34, 27, 99, 50};
+        Observable<Integer> source2 = Observable.fromArray(obj2).filter(data -> data%2 == 0);
+        source2.subscribe(System.out::println);
+
+        Single<Integer> single;
+        Observable<Integer> observable;
+
+        //first
+        single = Observable.fromArray(obj2).first(-1);
+        single.subscribe(data -> System.out.println("first() value " + data));
+
+        //last
+        single = Observable.fromArray(obj2).last(999);
+        single.subscribe(data -> System.out.println("last() value " + data));
+
+        //take
+        observable = Observable.fromArray(obj2).take(3);
+        observable.subscribe(data -> System.out.println("take(3) values " + data));
+
+        //takeLast
+        observable = Observable.fromArray(obj2).takeLast(3);
+        observable.subscribe(data -> System.out.println("takeLast(3) values " + data));
+
+        //skip
+        observable = Observable.fromArray(obj2).skip(2);
+        observable.subscribe(data -> System.out.println("skip(2) values " + data));
+
+        //skipLast
+        observable = Observable.fromArray(obj2).skipLast(2);
+        observable.subscribe(data -> System.out.println("skipLast(2) values " + data));
     }
 }
