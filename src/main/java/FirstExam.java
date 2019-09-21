5703f6e1bd6712bdbe7cc510ec260612af227e83
seung min <3235tmdals@gmail.com>
2019-09-21 22:33:18 +0900

    simple data query

--- src/main/java/FirstExam.java
+++ src/main/java/FirstExam.java
@@ -1,16 +1,25 @@
 import io.reactivex.Maybe;
 import io.reactivex.Observable;
-import io.reactivex.Single;
-import io.reactivex.functions.BiFunction;
-import io.reactivex.internal.operators.observable.ObservableError;
+import org.apache.commons.lang3.tuple.Pair;
 
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FirstExam {
     public static void main(String[] args) {
-        BiFunction<String, String , String> mergeBalls = (ball1, ball2) -> ball2 + "(" + ball1 + ")";
-        String[] balls = {"1", "2", "3"};
-        Maybe<String> source = Observable.fromArray(balls).reduce(mergeBalls);
-        source.subscribe(System.out::println);
+        //1. input data
+        //item name on left, price on right
+        List<Pair<String, Integer>> sales = new ArrayList<>();
+
+        sales.add(Pair.of("TV", 2500));
+        sales.add(Pair.of("Camera", 300));
+        sales.add(Pair.of("TV", 1600));
+        sales.add(Pair.of("phone", 800));
+
+        Maybe<Integer> tvSales = Observable.fromIterable(sales)
+                .filter(sale -> "TV".equals(sale.getLeft()))
+                .map(sale -> sale.getRight())
+                .reduce((sale1, sale2) -> sale1 + sale2);
+        tvSales.subscribe(tot -> System.out.println("TV Sales: $" + tot));
     }
 }
