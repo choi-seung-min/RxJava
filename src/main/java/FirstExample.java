4e63fa0b21b951e6f5bbcb54c76c848c0b33042b
seung min <3235tmdals@gmail.com>
2019-09-28 22:40:05 +0900

    interval

--- src/main/java/FirstExample.java
+++ src/main/java/FirstExample.java
@@ -5,9 +5,10 @@ import java.util.concurrent.TimeUnit;
 public class FirstExample {
     public static void main(String[] args) {
         CommonUtils.exampleStart();
-        Observable<Long> source = Observable.interval(100L, TimeUnit.MILLISECONDS)
-                .map(data -> (data + 1) * 100)
+        Observable<Long> source = Observable.interval(0L,100L, TimeUnit.MILLISECONDS)
+                .map(val -> val + 100)
                 .take(5);
         source.subscribe(Log::it);
+        CommonUtils.sleep(1000);
     }
 }
