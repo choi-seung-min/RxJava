6c83ef177a57d7e9001a73751ec512b84b34ba73
seung min <3235tmdals@gmail.com>
2019-09-29 22:37:14 +0900

    concatMap()

--- src/main/java/FirstExample.java
+++ src/main/java/FirstExample.java
@@ -4,13 +4,21 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+
 public class FirstExample {
     public static void main(String[] args) {
-        String[] balls = {"1", "3", "5"};
-        Observable<String> source = Observable.fromArray(balls)
-                .repeat(3);
+        CommonUtils.exampleStart();
 
-        source.doOnComplete(() -> System.out.println("onComplete\n\n"))
-                .subscribe(Log::it);
+        String[] balls = {"1", "3", "5"};
+        Observable<String> source = Observable.interval(100L, TimeUnit.MILLISECONDS)
+                .map(Long::intValue)
+                .map(idx -> balls[idx])
+                .take(balls.length)
+                .concatMap(ball -> Observable.interval(200L, TimeUnit.MILLISECONDS)
+                .map(notUsed -> ball + "<>")
+                .take(2)
+                );
+        source.subscribe(Log::it);
+        CommonUtils.sleep(2000);
     }
 }
