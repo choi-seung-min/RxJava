a5bcea341c4fdcb8f5fc7680ea18c3ea742793b5
seung min <3235tmdals@gmail.com>
2019-09-29 19:33:38 +0900

    intervalRange and how to make intervalRange with interval

--- src/main/java/FirstExample.java
+++ src/main/java/FirstExample.java
@@ -6,8 +6,13 @@ import java.util.concurrent.TimeUnit;
 
 public class FirstExample {
     public static void main(String[] args) {
-        Observable<Integer> source = Observable.range(1, 10)
-                .filter(num -> num % 2 == 0);
+        //source and source2 is same
+        Observable<Long> source = Observable.intervalRange(1, 5, 100L, 100L, TimeUnit.MILLISECONDS);
+        Observable<Long> source2 = Observable.interval(100L, 100L, TimeUnit.MILLISECONDS)
+                .map(val -> val + 1)
+                .take(5);
         source.subscribe(Log::it);
+        source2.subscribe(Log::it);
+        CommonUtils.sleep(1000);
     }
 }
