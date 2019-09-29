9f091d86e378b42232b815edb115b6a79813f0b2
seung min <3235tmdals@gmail.com>
2019-09-29 19:25:28 +0900

    range()

--- src/main/java/FirstExample.java
+++ src/main/java/FirstExample.java
@@ -1,14 +1,13 @@
 import io.reactivex.Observable;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 public class FirstExample {
     public static void main(String[] args) {
-        CommonUtils.exampleStart();
-        Observable<Long> source = Observable.interval(0L,100L, TimeUnit.MILLISECONDS)
-                .map(val -> val + 100)
-                .take(5);
+        Observable<Integer> source = Observable.range(1, 10)
+                .filter(num -> num % 2 == 0);
         source.subscribe(Log::it);
-        CommonUtils.sleep(1000);
     }
 }
--- src/main/java/Log.java
+++ src/main/java/Log.java
@@ -1,6 +1,6 @@
 public class Log {
     public static void it(Object obj){
         long time = System.currentTimeMillis() - CommonUtils.startTime;
-        System.out.println("RxJava | " + time + "|" + "value" + obj);
+        System.out.println("RxJava|" + time + "|" + "value: " + obj);
     }
 }
