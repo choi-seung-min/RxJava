b8bf2cf83e3eb77bb9f60285814c638fde2b9881
seung min <3235tmdals@gmail.com>
2019-10-18 22:13:58 +0900

    scan()

--- src/main/java/FirstExample.java
+++ src/main/java/FirstExample.java
@@ -8,14 +8,9 @@ import java.util.concurrent.TimeUnit;
 
 public class FirstExample {
     public static void main(String[] args) {
-        String[] objs = {"6", "4", "2-T", "2", "6-T", "4-T"};
-        Observable<GroupedObservable<String, String >> source =
-                Observable.fromArray(objs).groupBy(CommonUtils::getShape);
-
-        source.subscribe(obj -> {
-            obj.subscribe(
-                    val -> System.out.println("GROUP: " + obj.getKey() + "\t Value: " + val)
-            );
-        });
+        String[] balls = {"1", "2", "3"};
+        Observable<String> source = Observable.fromArray(balls)
+                .scan((ball1, ball2) -> ball2 + "(" + ball1 + ")");
+        source.subscribe(System.out::println);
     }
 }
