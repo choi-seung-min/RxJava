108ea2ef8c674e4d1bdff56bda1d21ea4d7d8997
seung min <3235tmdals@gmail.com>
2019-12-05 19:53:25 +0900

    amb()

--- src/main/java/FirstExample.java
+++ src/main/java/FirstExample.java
@@ -6,28 +6,29 @@ import io.reactivex.observables.ConnectableObservable;
 import io.reactivex.observables.GroupedObservable;
 
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 
 public class FirstExample {
     public static void main(String[] args) {
-        Action onCompleteAction = () -> System.out.println("onComplete");
-
         String[] data1 = {"1", "3", "5"};
-        String[] data2 = {"2", "4", "6"};
+        String[] data2 = {"2-R", "4-R"};
 
-        Observable<String> source1 = Observable.fromArray(data1)
-                .doOnComplete(onCompleteAction);
-        Observable<String> source2 = Observable.interval(100L, TimeUnit.MILLISECONDS)
-                .map(Long::intValue)
-                .map(idx -> data2[idx])
-                .take(data2.length)
-                .doOnComplete(onCompleteAction);
+        List<Observable<String>> sources = Arrays.asList(
+                Observable.fromArray(data1)
+                .doOnComplete(() -> System.out.println("Observable #1 : onComplete()")),
+                Observable.fromArray(data2)
+                .delay(100L, TimeUnit.MILLISECONDS)
+                .doOnComplete(() -> System.out.println("Observable #2 : onComplete()"))
+        );
 
-        Observable<String> source = Observable.concat(source1, source2).doOnComplete(onCompleteAction);
-        source.subscribe(System.out::println);
+        Observable.amb(sources)
+                .doOnComplete(() -> System.out.println("Result : onComplete()"))
+                .subscribe(System.out::println);
         CommonUtils.sleep(1000);
     }
 }
