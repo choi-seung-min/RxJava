1b7f947f290d01c50b7c7028eef8442f92a4db12
seung min <3235tmdals@gmail.com>
2019-12-06 21:48:06 +0900

    timeInterval()

--- src/main/java/CommonUtils.java
+++ src/main/java/CommonUtils.java
@@ -1,9 +1,19 @@
+import java.util.Random;
+
 public class CommonUtils {
 
     public static long startTime;
 
     public static void exampleStart(){
         startTime = System.currentTimeMillis();
+    }
+
+    public static void doSomething(){
+        try {
+            Thread.sleep(new Random().nextInt(100));
+        } catch (InterruptedException e){
+            e.printStackTrace();
+        }
     }
 
     public static void sleep(int millis){
--- src/main/java/FirstExample.java
+++ src/main/java/FirstExample.java
@@ -4,6 +4,7 @@ import io.reactivex.*;
 import io.reactivex.functions.Action;
 import io.reactivex.observables.ConnectableObservable;
 import io.reactivex.observables.GroupedObservable;
+import io.reactivex.schedulers.Timed;
 
 import java.sql.Time;
 import java.text.SimpleDateFormat;
@@ -16,9 +17,16 @@ import java.util.concurrent.TimeUnit;
 
 public class FirstExample {
     public static void main(String[] args) {
-        String[] data = {"1", "7", "2", "3", "4"};
-        Observable<String> source = Observable.fromArray(data)
-                .delay(100L, TimeUnit.MILLISECONDS);
+        String[] data = {"1", "3", "7"};
+
+        CommonUtils.exampleStart();
+        Observable<Timed<String>> source = Observable.fromArray(data)
+                .delay(item -> {
+                    CommonUtils.doSomething();
+                    return Observable.just(item);
+                })
+                .timeInterval();
+
         source.subscribe(System.out::println);
         CommonUtils.sleep(1000);
     }
