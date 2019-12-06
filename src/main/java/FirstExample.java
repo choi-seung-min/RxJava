109d062fa0b1e1216f2fd746fa1c23ce7202b764
seung min <3235tmdals@gmail.com>
2019-12-06 20:16:13 +0900

    Rxjava2Extension

--- src/main/java/FirstExample.java
+++ src/main/java/FirstExample.java
@@ -1,6 +1,6 @@
 import com.sun.tools.jdeprscan.scan.Scan;
-import io.reactivex.Observable;
-import io.reactivex.ObservableEmitter;
+import hu.akarnokd.rxjava2.math.MathFlowable;
+import io.reactivex.*;
 import io.reactivex.functions.Action;
 import io.reactivex.observables.ConnectableObservable;
 import io.reactivex.observables.GroupedObservable;
@@ -16,13 +16,30 @@ import java.util.concurrent.TimeUnit;
 
 public class FirstExample {
     public static void main(String[] args) {
-        String[] data = {"1", "2", "3", "4", "5", "6"};
+        Integer[] data = {1, 2, 3,4 };
 
-        Observable<String> source = Observable.fromArray(data)
-                .zipWith(Observable.interval(100L, TimeUnit.MILLISECONDS), (val, time) -> val)
-                .skipUntil(Observable.timer(500L, TimeUnit.MILLISECONDS));
+        //count
+        Single<Long> source = Observable.fromArray(data)
+                .count();
+        source.subscribe(count -> System.out.println("Count: " + count));
 
-        source.subscribe(System.out::println);
-        CommonUtils.sleep(1000);
+        //min & max
+        Flowable.fromArray(data)
+                .to(MathFlowable::max)
+                .subscribe(max -> System.out.println("Max: " + max));
+
+        Flowable.fromArray(data)
+                .to(MathFlowable::min)
+                .subscribe(min -> System.out.println("Min: " + min));
+
+        //sun & average
+        Flowable<Integer> flowable = Flowable.fromArray(data)
+                .to(MathFlowable::sumInt);
+        flowable.subscribe(sum -> System.out.println("Sum: " + sum));
+
+        Flowable<Double> flowable1 = Observable.fromArray(data)
+                .toFlowable(BackpressureStrategy.BUFFER)
+                .to(MathFlowable::averageDouble);
+        flowable1.subscribe(avg -> System.out.println("Avg: " + avg));
     }
 }
