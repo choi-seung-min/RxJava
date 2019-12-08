c9bcb3a145a03a8d85ee4f807893bff4013a0935
seung min <3235tmdals@gmail.com>
2019-12-08 23:13:53 +0900

    Schedules.io()

--- src/main/java/FirstExample.java
+++ src/main/java/FirstExample.java
@@ -7,6 +7,7 @@ import io.reactivex.observables.GroupedObservable;
 import io.reactivex.schedulers.Schedulers;
 import io.reactivex.schedulers.Timed;
 
+import java.io.File;
 import java.sql.Time;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
@@ -18,17 +19,14 @@ import java.util.concurrent.TimeUnit;
 
 public class FirstExample {
     public static void main(String[] args) {
-        String[] orgs = {"1", "3", "5"};
-        Observable<String> source = Observable.fromArray(orgs)
-                .zipWith(Observable.interval(100L, TimeUnit.MILLISECONDS), (a, b) -> a);
+        String root = "c:\\";
+        File[] files = new File(root).listFiles();
+        Observable<String> source = Observable.fromArray(files)
+                .filter(f -> !f.isDirectory())
+                .map(f -> f.getAbsolutePath())
+                .subscribeOn(Schedulers.io());
 
-        source.map(item -> "<<" + item + ">>")
-                .subscribeOn(Schedulers.computation())
-                .subscribe(Log::it);
-
-        source.map(item -> "##" + item + "##")
-                .subscribeOn(Schedulers.computation())
-                .subscribe(Log::it);
-        CommonUtils.sleep(1000);
+        source.subscribe(Log::it);
+        CommonUtils.sleep(500);
     }
 }
