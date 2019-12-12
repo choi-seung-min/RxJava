00d82bf49ad86317671d539fd34ec501bd476564
seung min <3235tmdals@gmail.com>
2019-12-12 10:44:22 +0900

    Callback hell example

--- src/main/java/FirstExample.java
+++ src/main/java/FirstExample.java
@@ -6,8 +6,10 @@ import io.reactivex.observables.ConnectableObservable;
 import io.reactivex.observables.GroupedObservable;
 import io.reactivex.schedulers.Schedulers;
 import io.reactivex.schedulers.Timed;
+import okhttp3.*;
 
 import java.io.File;
+import java.io.IOException;
 import java.sql.Time;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
@@ -20,15 +22,48 @@ import java.util.concurrent.TimeUnit;
 
 
 public class FirstExample {
+
+    private final OkHttpClient client = new OkHttpClient();
+
+    private static final String FIRST_URL = "https://api.github.com/zen";
+    private static final String SECOND_URL = CommonUtils.GITHUB_ROOT + "/samples/callback_hell";
+
+    private Callback onSuccess = new Callback() {
+        @Override
+        public void onFailure(Call call, IOException e) {
+            e.printStackTrace();
+        }
+
+        @Override
+        public void onResponse(Call call, Response response) throws IOException {
+            Log.i(response.body().string());
+        }
+    };
+
+    public void run(){
+        Request request = new Request.Builder()
+                .url(FIRST_URL)
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                Log.i(response.body().string());
+
+                Request request = new Request.Builder()
+                        .url(SECOND_URL)
+                        .build();
+                client.newCall(request).enqueue(onSuccess);
+            }
+        });
+    }
+
     public static void main(String[] args) {
-        final int THREAD_NUM = 10;
-
-        String[] data = {"1", "3", "5"};
-        Observable<String> source = Observable.fromArray(data);
-        Executor executor = Executors.newFixedThreadPool(THREAD_NUM);
-
-        source.subscribeOn(Schedulers.from(executor)).subscribe(Log::it);
-        source.subscribeOn(Schedulers.from(executor)).subscribe(Log::it);
-        CommonUtils.sleep(500);
+        FirstExample example = new FirstExample();
+        example.run();
     }
 }
