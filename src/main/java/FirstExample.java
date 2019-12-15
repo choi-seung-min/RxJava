9104138aee6046f98270d7f800f216cc255b0f01
seung min <3235tmdals@gmail.com>
2019-12-15 21:48:53 +0900

    clean import

--- src/main/java/FirstExample.java
+++ src/main/java/FirstExample.java
@@ -1,25 +1,6 @@
-import com.sun.tools.jdeprscan.scan.Scan;
-import hu.akarnokd.rxjava2.math.MathFlowable;
 import io.reactivex.*;
-import io.reactivex.functions.Action;
-import io.reactivex.observables.ConnectableObservable;
-import io.reactivex.observables.GroupedObservable;
 import io.reactivex.schedulers.Schedulers;
-import io.reactivex.schedulers.Timed;
 import okhttp3.*;
-
-import java.io.File;
-import java.io.IOException;
-import java.sql.Time;
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
-import java.util.Scanner;
-import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
-
 
 public class FirstExample {
 
